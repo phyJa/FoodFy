161f6d14574a43c3c260140670c6f85377ad3599
Elias <eli.bueno9@gmail.com>
2020-09-25 09:57:40 -0300

    Data created

---
+++ data/data.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        recipeTitle: "Triplo Bacon Burguer",
+        recipeOwner: "Jorge Relato",
+        imgSrc: "/assets/burger.png"
+    },
+    {
+        recipeTitle: "Pizza Quatro Estações",
+        recipeOwner: "Fabiana Melo",
+        imgSrc: "/assets/pizza.png"
+    },
+    {
+        recipeTitle: "Espaguete ao alho",
+        recipeOwner: "Fabiana Kinoto",
+        imgSrc: "/assets/espaguete.png"
+    },
+    {
+        recipeTitle: "Lasanha mac n' cheese",
+        recipeOwner: "Juliano Vieira",
+        imgSrc: "/assets/lasanha.png"
+    },
+    {
+        recipeTitle: "Docinhos pão-do-céu",
+        recipeOwner: "Ricardo Golvea",
+        imgSrc: "/assets/doce.png"
+    },
+    {
+        recipeTitle: "Asinhas de frango ao barbecue",
+        recipeOwner: "Vania Steroski",
+        imgSrc: "/assets/asinhas.png"
+    }
+];
