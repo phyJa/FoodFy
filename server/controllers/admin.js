6cc33f000504adde423d7a5755f687038192cc1c
Elias <eli.bueno9@gmail.com>
2021-01-29 09:53:01 -0300

    Admin routes created

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,7 @@
 const recipesData = require("../../data/data");
 
 module.exports = {
+    // Functions to render pages
     index(request, response) {
         return response.render(
             "admin/recipes",
@@ -8,5 +9,24 @@ module.exports = {
                 recipes: recipesData
             }
         );
+    },
+    create(request, response) {
+
+    },
+    show(request, response) {
+
+    },
+    edit(request, response) {
+
+    },
+    // Functions to perform actions
+    post(request, response) {
+
+    },
+    put(request, response) {
+
+    },
+    delete(request, response) {
+        
     }
 }
--- server/routes.js
+++ server/routes.js
@@ -4,18 +4,22 @@ const routes = express.Router();
 
 // Visitor pages
 const visitorController = require("./controllers/visitor");
-// Landing page
-routes.get("/", visitorController.home);
-// Show recipes
-routes.get("/recipes", visitorController.index);
-// About
-routes.get("/about", visitorController.about);
-// A specific recipe
-routes.get("/recipes/:index", visitorController.show);
+routes.get("/", visitorController.home); // Landing page
+routes.get("/recipes", visitorController.index); // Show recipes
+routes.get("/about", visitorController.about); // About
+routes.get("/recipes/:index", visitorController.show); // A specific recipe
 
-// Admin pages
+// Admin
 const recipes = require("./controllers/admin");
-// List the recipes
+
+// Render pages
 routes.get("/admin/recipes", recipes.index);
+routes.get("/admin/recipes/create", recipes.create); // Show the form of a new recipe
+routes.get("/admin/recipes/:id", recipes.show); // Show details of a recipe
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Show the form to edit a recipe 
+// Actions
+routes.post("/admin/recipes", recipes.post); // Register a new recipe
+routes.put("/admin/recipes", recipes.put); // Edit a recipe
+routes.delete("/admin/recipes", recipes.delete); // Delete a recipe
 
 module.exports = routes;
