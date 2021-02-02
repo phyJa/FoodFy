d909ac0438e9af3e7cb778539bf201a8bdf6405b
Elias <eli.bueno9@gmail.com>
2021-02-01 21:49:39 -0300

    Recipes display almost ready

--- public/global/global.js
+++ public/global/global.js
@@ -3,21 +3,14 @@ const recipes = document.querySelectorAll(".recipe");
 
 // Add an event listener to each recipe
 for(let recipe of recipes) {
-
     recipe.addEventListener (
-
-        "click",
-
-        function () {
-           
+        "click", 
+        function()  {
             // Get the hidden recipe index passed by nunjucks
             const recipeIndex = recipe.querySelector("span").innerHTML;
-            
+                   
             // Change the browser page
-            window.location.href= `/recipes/${recipeIndex}`;
-            
-        }
-    
+            window.location.href= `/recipes/${recipeIndex}`;  
+       }
     )
-
 }
