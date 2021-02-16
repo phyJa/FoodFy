1c3c9bc1b28b7d4d7b2984bb8bc008190eae2863
Elias <eli.bueno9@gmail.com>
2021-02-16 01:31:17 -0300

    Add field dynamically for recipe preparation

--- public/scripts/dynamicRecipe.js
+++ public/scripts/dynamicRecipe.js
@@ -18,3 +18,24 @@ function addIngredient() {
         ingredientsContainer.appendChild(newRecipeButton);
     }
 }
+
+function addStep() {
+    const preparationContainer = document.querySelector(".recipePreparationContainer");
+    const preparation = preparationContainer.querySelectorAll(".preparation");
+
+    const lastFieldOfPreparation = preparation[preparation.length - 1].cloneNode(true);
+    
+    if(lastFieldOfPreparation.value === "")
+        return false;
+    else {
+        // Remove the button first from the container
+        const newStepButton = preparationContainer.querySelector(".buttonContainer");
+        preparationContainer.removeChild(newStepButton);
+        // Add the new input for the ingredient
+        const newField = lastFieldOfPreparation.cloneNode(true);
+        newField.value = "";
+        preparationContainer.appendChild(newField);
+        // Add the button again
+        preparationContainer.appendChild(newStepButton);
+    }
+}
