b4b14a595691f17346fd9d556c83036425f1c1f3
Elias <eli.bueno9@gmail.com>
2021-02-17 01:13:41 -0300

    Improve dynamic function to reuse code

--- public/scripts/dynamicRecipe.js
+++ public/scripts/dynamicRecipe.js
@@ -1,41 +1,20 @@
-function addIngredient() {
-    const ingredientsContainer = document.querySelector(".recipeIngredientsContainer");
-    const ingredients = ingredientsContainer.querySelectorAll(".ingredient");
+function addItem(containerClassName) {
+    const itemsContainer = document.querySelector(`.${containerClassName}`);
+    const items = itemsContainer.querySelectorAll("input");
 
-    const lastField = ingredients[ingredients.length - 1].cloneNode(true);
+    const lastField = items[items.length - 1].cloneNode(true);
     
     if(lastField.value === "")
         return false;
     else {
         // Remove the button first from the container
-        const newRecipeButton = ingredientsContainer.querySelector(".buttonContainer");
-        ingredientsContainer.removeChild(newRecipeButton);
+        const newItemButton = itemsContainer.querySelector(".buttonContainer");
+        itemsContainer.removeChild(newItemButton);
         // Add the new input for the ingredient
         const newField = lastField.cloneNode(true);
         newField.value = "";
-        ingredientsContainer.appendChild(newField);
+        itemsContainer.appendChild(newField);
         // Add the button again
-        ingredientsContainer.appendChild(newRecipeButton);
-    }
-}
-
-function addStep() {
-    const preparationContainer = document.querySelector(".recipePreparationContainer");
-    const preparation = preparationContainer.querySelectorAll(".preparation");
-
-    const lastFieldOfPreparation = preparation[preparation.length - 1].cloneNode(true);
-    
-    if(lastFieldOfPreparation.value === "")
-        return false;
-    else {
-        // Remove the button first from the container
-        const newStepButton = preparationContainer.querySelector(".buttonContainer");
-        preparationContainer.removeChild(newStepButton);
-        // Add the new input for the ingredient
-        const newField = lastFieldOfPreparation.cloneNode(true);
-        newField.value = "";
-        preparationContainer.appendChild(newField);
-        // Add the button again
-        preparationContainer.appendChild(newStepButton);
+        itemsContainer.appendChild(newItemButton);
     }
 }
