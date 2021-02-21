9cdd48b86c84a945188ddc193ff29b6123429963
Elias <eli.bueno9@gmail.com>
2021-02-21 00:29:47 -0300

    Add fields dynamically with close button

--- public/scripts/dynamicRecipe.js
+++ public/scripts/dynamicRecipe.js
@@ -1,19 +1,20 @@
 function addItem(containerClassName) {
     const itemsContainer = document.querySelector(`.${containerClassName}`);
-    const items = itemsContainer.querySelectorAll("input");
-
-    const lastField = items[items.length - 1].cloneNode(true);
+    const items = itemsContainer.querySelectorAll(".inputAndCloseButtonContainer");
+   
+    const lastContainer = items[items.length - 1].cloneNode(true);
+    const inputValueOfLastContainer = lastContainer.querySelector("input").value;
     
-    if(lastField.value === "")
+    if(inputValueOfLastContainer === "")
         return false;
     else {
         // Remove the button first from the container
         const newItemButton = itemsContainer.querySelector(".buttonContainer");
         itemsContainer.removeChild(newItemButton);
         // Add the new input for the ingredient
-        const newField = lastField.cloneNode(true);
-        newField.value = "";
-        itemsContainer.appendChild(newField);
+        const newContainer = lastContainer.cloneNode(true);
+        newContainer.querySelector("input").value = "";
+        itemsContainer.appendChild(newContainer);
         // Add the button again
         itemsContainer.appendChild(newItemButton);
     }
